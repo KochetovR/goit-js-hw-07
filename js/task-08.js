59517d9ff63aa9592595b96436da276641b5c0fa
Kochetov Roman <k04erg0@gmail.com>
2021-04-25 22:15:27 +0300

    add task 08

--- js/task-08.js
+++ js/task-08.js
@@ -3,26 +3,35 @@ const renderBtn = document.querySelector('[data-action="render"]')
 const destroyBtn = document.querySelector('[data-action="destroy"]')
 const collectionDiv = document.querySelector('#boxes')
 
+let w = 20;
+let h = 20;
+
 const createBoxes = (amount) => {
-    console.log(amount.currentTarget.value);
-
-    renderBtn.addEventListener('click', () => {
-        // const colorElement = Math.round(765.0 * Math.random());
-        const collectionEl = document.createElement('div')
+    
+    for (let i = 0; i < amount.currentTarget.value; i += 1){
+        w += 10;
+        h += 10;
+        let r = Math.round(255 * Math.random());
+        let g = Math.round(255 * Math.random());
+        let b = Math.round(255 * Math.random());
+        
+        let collectionEl = document.createElement('div')
            
-    // collectionEl.style.backgroundColor = `#${colorElement}`
-    collectionEl.style.width = '30px';
-    collectionEl.style.height = '30px'
-    console.log(collectionEl)
-
-    collectionDiv.append(collectionEl);  
+        collectionEl.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+        collectionEl.style.width = `${w}px`; 
+        collectionEl.style.height = `${h}px`;
+    
+        renderBtn.addEventListener('click', () => {
+        collectionDiv.append(collectionEl);  
+        })
+         destroyBtn.addEventListener('click', () => {
+            collectionDiv.remove();
+            inputEl.value = '';
     })
-    
-    
+    } 
 }
 
 inputEl.addEventListener('input', createBoxes)
 
-destroyBtn.addEventListener('click', () => {
-    collectionDiv.remove();
-})
+
+
